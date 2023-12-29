d98b4410d1969329f3bc1a42ec7437d072169ce5
Joe Randazzo <131551196+jprandazzo@users.noreply.github.com>
2023-12-29 15:21:20 -0800

    Update test-script.js

--- test-script.js
+++ test-script.js
@@ -1,3 +1,7 @@
 <script>
-  document.querySelector('h1').createElement('a').setAttribute('href', 'www.google.com');
+  let child = document.createElement('p');
+  const text = document.createTextNode('hello children');
+  child.appendChild(text);
+
+  document.querySelector('h1').appendChild(child)
 </script>
