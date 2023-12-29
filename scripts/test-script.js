6a05dd277446466e991a6bcda8d6e23cea255299
Joe Randazzo <131551196+jprandazzo@users.noreply.github.com>
2023-12-29 15:50:00 -0800

    Update test-script.js

--- scripts/test-script.js
+++ scripts/test-script.js
@@ -1,4 +1,8 @@
+console.log('test1');
+
 window.addEventListener('DOMContentLoaded', () => {
+  console.log('loaded');
+  
   const child = document.createElement('p');
   const text = document.createTextNode('hello children');
   child.appendChild(text);
